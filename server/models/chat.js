3a374595650f6f7fe3d01575f65ef6cf2e69081f
D KIRAN KUMAR <93006743+KiranKumarDevapatla@users.noreply.github.com>
2024-02-11 20:44:32 +0530

    Delete server directory

--- server/models/chat.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const chat = new mongoose.Schema({
-    message: String,
-    createdAt: {type:Date, default:Date.now},
-    id: Number,
-},{timestamps:true});
-
-module.exports = chat;
